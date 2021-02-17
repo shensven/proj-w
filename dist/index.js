f365864f4df13cd8ba0f598843fe2412b00e7f5b
shensven <shensven2016@gmail.com>
2021-02-17 23:30:42 +0800

    Drop .code-workspace

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,14 @@
 "use strict";
 
 function sendMsg() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("get", "https://devapi.qweather.com/v7/weather/now?location=101290116&key=7e3a22824fc74b4bb1fcde68d101f195&lang=en");
-    xhr.send();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log(xhr.responseText);
-            }
-        }
-    };
+  var xhr = new XMLHttpRequest();
+  xhr.open("get", "https://devapi.qweather.com/v7/weather/now?location=101290116&key=7e3a22824fc74b4bb1fcde68d101f195&lang=en");
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      }
+    }
+  };
 }
