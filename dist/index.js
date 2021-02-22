108725838d3e515066451c8b0437746103b4eb44
shensven <shensven2016@gmail.com>
2021-02-22 21:57:59 +0800

    Install Vue

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,26 @@
 "use strict";
 
-function sendMsg() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("get", "https://devapi.qweather.com/v7/weather/now?location=101290116&key=7e3a22824fc74b4bb1fcde68d101f195&lang=en");
-  xhr.send();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var resp_json = JSON.parse(this.responseText);
-      console.log(resp_json);
-      document.querySelector("#preloader").innerHTML = "\n        <p>" + resp_json.now.text + "</p>      \n      ";
-    }
-  };
-}
+// function sendMsg() {
+//   let xhr = new XMLHttpRequest();
+//   xhr.open(
+//     "get",
+//     "https://devapi.qweather.com/v7/weather/now?location=101290116&key=7e3a22824fc74b4bb1fcde68d101f195&lang=en"
+//   );
+//   xhr.send();
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//       let resp_json = JSON.parse(this.responseText);
+//       console.log(resp_json);
+//       document.querySelector("#preloader").innerHTML = `
+//         <p>${resp_json.now.text}</p>
+//       `;
+//     }
+//   };
+// }
+
+var app = new Vue({
+  el: "#preloader",
+  data: {
+    preloader: "Hello World!"
+  }
+});
