c3c3337413e054e81378ab9e2b09b4a6dfeac784
shensven <shensven2016@gmail.com>
2021-03-01 22:09:28 +0800

    npm install axios

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <HelloWorld />
+    <button @click="getCurrentWeather">现在天气</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
+  },
+  methods: {
+    getCurrentWeather: function() {
+      axios
+        .get("https://devapi.qweather.com/v7/weather/now", {
+          params: {
+            location: 101290116,
+            key: "7e3a22824fc74b4bb1fcde68d101f195",
+            lang: "en"
+          }
+        })
+        .then(response => console.log(response));
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
